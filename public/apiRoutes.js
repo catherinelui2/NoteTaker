94bd0952859a8470079512385556fca8b86eca39
CatL <catherinelui.2@gmail.com>
2020-09-08 18:25:04 -0600

    added id to each note and add delete function. this is

--- public/apiRoutes.js
+++ public/apiRoutes.js
@@ -1,15 +1,75 @@
-const noteData = require("../db/db.json");
+const path = require("path");
+const fs = require("fs");
 
-module.exports = function(app) {
-    app.get("/api/notes", function (req, res) {
+let noteData = [];
+
+module.exports = function (app) {
+    app.get("/api/notes", function (err, res) {
+        try {
+            //read existing db file before rendering
+            noteData = fs.readFileSync("./db/db.json", "utf8");
+            //parse noteData to turn it into an array of objects
+            noteData = JSON.parse(noteData);
+        } catch (err) {
+            console.log("\n error (in app.get.catch):");
+            console.log("err");
+        }
+        //return objects to the broswer
         res.json(noteData);
     });
 
+    //post a new object to db.json file
     app.post("/api/notes", function (req, res) {
-        noteData.push(req.body);
-        res.json(noteData);
+        try {
+            //asking the server to read json file
+            noteData = fs.readFileSync("./db/db.json", "utf8");
+            console.log(noteData);
+
+            //parse the data to get an array of objects
+            noteData = JSON.parse(noteData);
+            //set id for each entry
+            req.body.id = noteData.length;
+            //push user input to the arrary
+            noteData.push(req.body);
+            //stringify before writting it to the JSON file
+            noteData = JSON.stringify(noteData);
+            //write the new note to the file
+            fs.writeFile("./db/db.json", noteData, "utf8", function(err) {
+                if (err) throw err;
+            });
+            //change to to an array of objects and render it to the broswer
+            res.json(JSON.parse(noteData));
+        } catch (err) {
+            throw err;
+            console.log(err);
+        }
+        // var newNote = req.body;
+        // newNote.id = uuidv4();
+        // noteData.push(newNote);
+        // fs.writeFile(noteData);
+        // res.json(newNote);
     });
 
-    // app.delete("/api/notes/:id", )
-}
+    // to allow user to delete a note by id
+    app.delete("/api/notes/:id", function (req, res) {
+        try {
+            noteData = fs.readFileSync("./db/db.json", "utf8");
 
+            noteData = JSON.parse(noteData);
+            noteData = noteData.filter(function (note) {
+                return note.id != req.params.id;
+            });
+
+            noteData = JSON.stringify(noteData);
+
+            fs.writeFile("./db/db.json", noteData, "utf8", function (err) {
+                if (err) throw err;
+            });
+
+            res.send(JSON.parse(noteData));
+        } catch (err) {
+            throw err;
+            console.log(err);
+        }
+    });
+};
